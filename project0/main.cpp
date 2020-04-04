67dc7e05509b6f34af197910b1c5ad93815e4e18
zachwreed <reedz@oregonstate.edu>
2020-04-04 13:58:47 -0700

    adds source code for project0

---
+++ project0/main.cpp
@@ -0,0 +1,61 @@
+/************************************************
+ * Author: Zachary Reed
+ * Description: Project 0 source code
+ * Date: 4/6/2020
+ * References:
+ * 1.) Author: Mike Baily
+ *     Title: Simple OpenMP Experiment main Program
+ *     Description: Source code example for Project 0
+ *     Date Accessed: 4/5/2020
+ ************************************************/
+
+#include <omp.h>
+#include <stdio.h>
+#include <math.h>
+
+#define NUMT	         4
+#define SIZE       	16384
+#define NUMTRIES        1000
+
+float A[SIZE];
+float B[SIZE];
+float C[SIZE]
+
+int main() {
+#ifndef _OPENMP
+        fprintf(stderr, "OpenMP is not supported here -- sorry.\n");
+        return 1
+#endif
+
+	// inialize the arrays:
+	for (int i = 0; i < SIZE; i++) {
+		A[ i ] = 1.;
+		B[ i ] = 2.;
+	}
+
+        omp_set_num_threads( NUMT );
+        fprintf(stderr, "Using %d threads\n", NUMT);
+
+        double maxMegaMults = 0.;
+
+        for (int t = 0; t < NUMTRIES; t++) {
+                double time0 = omp_get_wtime();
+
+                #pragma omp parallel for
+                for (int i = 0; i < SIZE; i++) {
+                        C[i] = A[i] * B[i];
+                }
+
+                double time1 = omp_get_wtime( );
+                double megaMults = (double)SIZE/(time1-time0)/1000000.;
+                if (megaMults > maxMegaMults)
+                        maxMegaMults = megaMults;
+        }
+
+        printf("Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults);
+
+	// note: %lf stands for "long float", which is how printf prints a "double"
+	//        %d stands for "decimal integer", not "double"
+
+        return 0;
+}
